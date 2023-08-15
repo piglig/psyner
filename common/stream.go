f1fa93d3ef1990f5ec88952a65f2042b248a1180
piglig <zhu1197437384@gmail.com>
2023-08-15 16:42:12 +0800

    feat: read data from stream (#23)

--- common/stream.go
+++ common/stream.go
@@ -1,6 +1,10 @@
 package common
 
-import "sync/atomic"
+import (
+	"encoding/binary"
+	"net"
+	"sync/atomic"
+)
 
 type Stream struct {
 	Incoming chan<- *Packet
@@ -22,4 +26,54 @@ func NewStream(bufferSize int) *Stream {
 	s.Incoming = s.in
 	s.Outgoing = s.out
 	return s
+}
+
+func (s *Stream) SetConnection(conn net.Conn) {
+	if conn == nil {
+		panic("SetConnection is nil")
+	}
+
+	s.value.Store(conn)
+
+	go s.read(conn)
+	go s.write(conn)
+}
+
+func (s *Stream) Connection() net.Conn {
+	return s.value.Load().(net.Conn)
+}
+
+func (s *Stream) read(conn net.Conn) {
+	var op FileSyncOp
+
+	var length int64
+	for {
+		err := binary.Read(conn, binary.BigEndian, &op)
+		if err != nil {
+			return
+		}
+
+		err = binary.Read(conn, binary.BigEndian, &length)
+		if err != nil {
+			return
+		}
+
+		data := make([]byte, length)
+		readLen := 0
+		n := 0
+		for readLen < len(data) {
+			n, err = conn.Read(data[readLen:])
+			if err != nil {
+				return
+			}
+
+			readLen += n
+		}
+
+		s.in <- NewPacket(op, data)
+	}
+}
+
+func (s *Stream) write(conn net.Conn) {
+
 }
